0963a98e4ccec22a1bd63584811c11a8fcc3bc38
masayoshi <omc61194@hotmail.com>
2022-07-16 23:23:21 +0900

    ✨ 講座5の完成時点まで

---
+++ Context02/src/components/TodoCounter.tsx
@@ -0,0 +1,8 @@
+import { FC, useContext } from "react";
+import { TodoContext } from "src/pages/_app";
+
+export const TodoCounter: FC = () => {
+  const { todos } = useContext(TodoContext);
+
+  return <h2>TODO: {todos.length}件</h2>;
+};
